7b639094ca4a91a3d903ee80e0439fcda6f10b21
MartMbithi <martdevelopers254@gmail.com>
2021-01-27 11:02:55 +0300

    Administrative Staff Dashboard Scaffoldings

--- partials/head.php
+++ partials/head.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>iCollege Information Management System</title>
     <link rel="icon" type="image/x-icon" href="../public/img/favicon.ico" />
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
     <link href="../public/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="../public/css/plugins.css" rel="stylesheet" type="text/css" />
     <link href="../public/css/authentication/form-1.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="../public/css/forms/theme-checkbox-radio.css">
     <link rel="stylesheet" type="text/css" href="../public/css/forms/switches.css">
+    <link href="../public/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+    <!-- Loader -->
+    <link href="../public/css/loader.css" rel="stylesheet" type="text/css" />
+    <script src="../public/js/loader.js"></script>
+    <link href="../public/plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
+    <link href="../public/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+
 </head>
--- partials/scripts.php
+++ partials/scripts.php
@@ -1,5 +1,17 @@
-<script src="../pubic/js/libs/jquery-3.1.1.min.js"></script>
-<script src="../pubic/plugins/bootstrap/js/popper.min.js"></script>
-<script src="../pubic/plugins/bootstrap/js/bootstrap.min.js"></script>
 <!-- Auth Js -->
 <script src="../public/js/authentication/form-1.js"></script>
+<!-- Dashboard Js -->
+<script src="../public/js/custom.js"></script>
+<script src="../public/js/libs/jquery-3.1.1.min.js"></script>
+<script src="../public/plugins/bootstrap/js/popper.min.js"></script>
+<script src="../public/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="../public/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="../public/js/app.js"></script>
+<script>
+    $(document).ready(function() {
+        App.init();
+    });
+</script>
+<script src="../public/js/custom.js"></script>
+<script src="../public/plugins/apex/apexcharts.min.js"></script>
+<script src="../public/js/dashboard/dash_2.js"></script>
