b3f86c982080b6384797f27de29df7a038091520
MartMbithi <martdevelopers254@gmail.com>
2021-01-27 11:57:40 +0300

    Data Tables Init

--- partials/head.php
+++ partials/head.php
@@ -8,16 +8,25 @@
     <title>iCollege Information Management System</title>
     <link rel="icon" type="image/x-icon" href="../public/img/favicon.ico" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <!-- Bootstrap -->
     <link href="../public/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Plugis -->
     <link href="../public/css/plugins.css" rel="stylesheet" type="text/css" />
+    <!-- Auth Forms -->
     <link href="../public/css/authentication/form-1.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="../public/css/forms/theme-checkbox-radio.css">
     <link rel="stylesheet" type="text/css" href="../public/css/forms/switches.css">
+    <!-- Dashboard  -->
     <link href="../public/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+    <!-- Loaders -->
     <link href="../public/css/loader.css" rel="stylesheet" type="text/css" />
     <script src="../public/js/loader.js"></script>
+    <!-- Apex Charts -->
     <link href="../public/plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
-    <link href="../public/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" />
+    <!-- Account Settings -->
     <link href="../public/css/users/account-setting.css" rel="stylesheet" type="text/css" />
+    <!-- Data Tables -->
+    <link rel="stylesheet" type="text/css" href="../public/plugins/table/datatable/datatables.css">
+    <link rel="stylesheet" type="text/css" href="../public/plugins/table/datatable/dt-global_style.css">
 
 </head>
