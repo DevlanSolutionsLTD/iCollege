29e4c35170217c0c83542673b29a779d0e6508e8
MartMbithi <martdevelopers254@gmail.com>
2021-02-02 10:34:31 +0300

    Front End Student Form

--- admin/students.php
+++ admin/students.php
@@ -286,7 +286,7 @@ require_once '../partials/head.php';
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
                                 <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                                <li class="breadcrumb-item active" aria-current="page"><span>Lecturers</span></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Students</span></li>
                             </ol>
                         </nav>
 
@@ -315,18 +315,18 @@ require_once '../partials/head.php';
                     <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
                         <div class="widget-content widget-content-area br-6">
                             <div class="text-right">
-                                <button data-toggle="modal" data-target="#import_lec" class="btn btn-outline-primary mb-2">Import Lecturers </button>
-                                <button data-toggle="modal" data-target="#add_lec" class="btn btn-outline-secondary mb-2">Add Lecturer</button>
+                                <button data-toggle="modal" data-target="#import_student" class="btn btn-outline-primary mb-2">Import Students </button>
+                                <button data-toggle="modal" data-target="#add_student" class="btn btn-outline-secondary mb-2">Add Student</button>
                             </div>
                             <hr>
                             <!-- Import Modals -->
-                            <div class="modal animated zoomInUp custo-zoomInUp" id="import_lec" role="dialog">
+                            <div class="modal animated zoomInUp custo-zoomInUp" id="import_studuent" role="dialog">
                                 <div class="modal-dialog modal-lg" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h4 class="text-center">
                                                 Allowed file types: XLS, XLSX.
-                                                <a class="text-primary" target="_blank" href="../public/templates/Lecturers.xlsx">Download</a> A Sample File.
+                                                <a class="text-primary" target="_blank" href="../public/templates/Students.xlsx">Download</a> A Sample File.
                                             </h4>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
@@ -361,8 +361,8 @@ require_once '../partials/head.php';
                             </div>
                             <!-- End Import Modal -->
 
-                            <!-- Add Course Modal -->
-                            <div class="modal animated zoomInUp custo-zoomInUp" id="add_lec" role="dialog">
+                            <!-- Add  Modal -->
+                            <div class="modal animated zoomInUp custo-zoomInUp" id="add_student" role="dialog">
                                 <div class="modal-dialog modal-xl" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
@@ -379,33 +379,45 @@ require_once '../partials/head.php';
                                                 <div class="card-body">
                                                     <div class="row">
                                                         <div class="form-group col-md-6">
-                                                            <label for="">Lecturer Number</label>
-                                                            <input type="text" required name="number" value="<?php echo $a; ?>-<?php echo $b; ?>" class="form-control">
+                                                            <label for="">Admission Number</label>
+                                                            <input type="text" required name="admno" value="<?php echo $a; ?>-<?php echo $b; ?>" class="form-control">
                                                             <!-- Hide This -->
                                                             <input type="hidden" required name="id" value="<?php echo $ID; ?>" class="form-control">
                                                         </div>
                                                         <div class="form-group col-md-6">
-                                                            <label for="">Lecturer Name</label>
+                                                            <label for="">Name</label>
                                                             <input type="text" required name="name" class="form-control">
                                                         </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Lecturer National ID Number</label>
+
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">National ID Number</label>
                                                             <input type="text" required name="idno" class="form-control">
                                                         </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Lecturer Phone Number</label>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Phone Number</label>
                                                             <input type="text" required name="phone" class="form-control">
                                                         </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Lecturer Email Address</label>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Phone Number</label>
+                                                            <select name="sex" class="form-control">
+                                                                <option>Male</option>
+                                                                <option>Female</option>
+                                                            </select>
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Address</label>
+                                                            <input type="text" required name="adr" class="form-control">
+                                                        </div>
+                                                        <div class="form-group col-md-4">
+                                                            <label for=""> Email Address</label>
                                                             <input type="text" required name="email" class="form-control">
                                                         </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Lecturer Password</label>
+                                                        <div class="form-group col-md-4">
+                                                            <label for="">Password</label>
                                                             <input type="text" required name="password" class="form-control">
                                                         </div>
                                                         <div class="form-group col-md-12">
-                                                            <label for="exampleInputFile">Lecturer Passport</label>
+                                                            <label for="exampleInputFile">Passport</label>
                                                             <div class="input-group">
                                                                 <div class="custom-file">
                                                                     <input required name="dpic" type="file" class="custom-file-input" id="exampleInputFile">
@@ -416,7 +428,7 @@ require_once '../partials/head.php';
                                                     </div>
                                                 </div>
                                                 <div class="text-right">
-                                                    <button type="submit" name="add_lec" class="btn btn-primary">Submit</button>
+                                                    <button type="submit" name="add_student" class="btn btn-primary">Submit</button>
                                                 </div>
                                             </form>
                                         </div>
@@ -427,9 +439,7 @@ require_once '../partials/head.php';
                                     </div>
                                 </div>
                             </div>
-                            <!-- End Course Modal -->
-
-                            <!-- End Course Modal -->
+                            <!-- End  Modal -->
 
                             <div class="table-responsive mb-4 mt-4">
                                 <table id="default-ordering" class="table" style="width:100%">
