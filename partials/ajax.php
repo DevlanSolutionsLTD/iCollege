d8b284accc45362809692e278406c14891ef29a5
MartMbithi <martdevelopers254@gmail.com>
2021-02-03 08:43:26 +0300

    Ajaxed Student Enrollments

---
+++ partials/ajax.php
@@ -0,0 +1,45 @@
+<?php
+include('../config/pdoconfig.php');
+
+/* Student Name */
+if (!empty($_POST["AdmissionNumber"])) {
+    $id = $_POST['AdmissionNumber'];
+    $stmt = $DB_con->prepare("SELECT * FROM iCollege_students WHERE admno = :id ");
+    $stmt->execute(array(':id' => $id));
+?>
+<?php
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+?>
+<?php echo htmlentities($row['name']); ?>
+<?php
+    }
+}
+
+
+/* Course Name */
+if (!empty($_POST["StudentName"])) {
+    $id = $_POST['StudentName'];
+    $stmt = $DB_con->prepare("SELECT * FROM iCollege_students WHERE admno = :id ");
+    $stmt->execute(array(':id' => $id));
+?>
+<?php
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+?>
+<?php echo htmlentities($row['course_name']); ?>
+<?php
+    }
+}
+
+/* Unit Code */
+if (!empty($_POST["UnitCode"])) {
+    $id = $_POST['UnitCode'];
+    $stmt = $DB_con->prepare("SELECT * FROM  iCollege_units WHERE code = :id ");
+    $stmt->execute(array(':id' => $id));
+?>
+<?php
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+?>
+<?php echo htmlentities($row['name']); ?>
+<?php
+    }
+}
--- partials/scripts.php
+++ partials/scripts.php
@@ -70,3 +70,43 @@
 <!-- Sweet Alerts -->
 <script src="../public/plugins/sweetalerts/sweetalert2.min.js"></script>
 <script src="../public/plugins/sweetalerts/custom-sweetalert.js"></script>
+<!-- Ajax -->
+<script>
+    /* Room Details Asyc */
+    function getStudentDetails(val) {
+        $.ajax({
+
+            type: "POST",
+            url: "../partials/ajax.php",
+            data: 'AdmissionNumber=' + val,
+            success: function(data) {
+                //alert(data);
+                $('#StudentName').val(data);
+            }
+        });
+
+        $.ajax({
+
+            type: "POST",
+            url: "../partials/ajax.php",
+            data: 'StudentName=' + val,
+            success: function(data) {
+                //alert(data);
+                $('#StudentCourse').val(data);
+            }
+        });
+
+        function getUnitDetails(val) {
+            $.ajax({
+
+                type: "POST",
+                url: "../partials/ajax.php",
+                data: 'UnitCode=' + val,
+                success: function(data) {
+                    //alert(data);
+                    $('#UnitName').val(data);
+                }
+            });
+        }
+    }
+</script>
