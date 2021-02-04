d13cce81899dcbdf1c5cdb1a6dacd6a734e90173
kilonzowambua <kilonzowambua254@gmail.com>
2021-02-04 10:53:50 +0300

    Timetable delete done

--- admin/timetables.php
+++ admin/timetables.php
@@ -316,7 +316,21 @@ if (isset($_POST['update'])) {
 }
 
 /* Delete Time Tables */
-
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $adn = 'DELETE FROM iCollege_timetable WHERE id=?';
+    $stmt = $conn->prepare($adn);
+    $stmt->bind_param('s', $id);
+    $stmt->execute();
+    $stmt->close();
+    if ($stmt) {
+        $success =
+            'Removed permantly' && header('refresh:1; url=timetables.php');
+    } else {
+        //inject alert that task failed
+        $info = 'Please Try Again Or Try Later';
+    }
+}
 require_once '../partials/head.php';
 ?>
 
@@ -644,7 +658,25 @@ require_once '../partials/head.php';
                                     </div>
                                 </div>
                             </div>
-                                                    <a href="#delete-<?php echo $tt->id; ?>" data-toggle="modal" class="badge outline-badge-danger">Delete</a>
+                                                    <a href="#delete-<?php echo $tt->id; ?>" data-toggle="modal" class="badge outline-badge-danger">Remove</a>
+                                                    <div class="modal animated zoomInUp custo-zoomInUp" id="delete-<?php echo $tt->id; ?>" role="dialog">
+                                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">CONFIRM</h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body text-center text-danger">
+                                                                    <h4>Remove  <?php echo $tt->unit_name; ?> from timetable ?</h4>
+                                                                    <br>
+                                                                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                    <a href="timetables.php?delete=<?php echo $tt->id; ?>" class="text-center btn btn-danger">Remove</a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         <?php }
