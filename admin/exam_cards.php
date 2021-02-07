c29aed8193690c589e14a8a2b7e1281da45bdb35
MartMbithi <martdevelopers254@gmail.com>
2021-02-07 11:12:00 +0300

    Implemented Examination Cards Module

---
+++ admin/exam_cards.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+require_once '../config/config.php';
+require_once '../config/checklogin.php';
+admin_check_login();
+require_once '../config/codeGen.php';
+require_once '../partials/head.php';
+?>
+
+<body>
+
+    <!--  BEGIN NAVBAR  -->
+    <?php require_once '../partials/admin_nav.php'; ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN NAVBAR  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg></a>
+
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
+                                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="">Exams</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Exam Cards</span></li>
+                            </ol>
+                        </nav>
+
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <?php require_once '../partials/admin_sidebar.php'; ?>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <div class="row layout-top-spacing">
+                    <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                        <div class="widget-content widget-content-area br-6">
+                            <div class="table-responsive mb-4 mt-4">
+                                <table id="default-ordering" class="table" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Admn No</th>
+                                            <th>Name</th>
+                                            <th>Unit Code</th>
+                                            <th>Unit Name</th>
+                                            <th>Semester Enrolled</th>
+                                            <th>Academic Yr Enrolled</th>
+                                            <th>Manage Enrollments</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $ret = 'SELECT * FROM `iCollege_enrollments`';
+                                        $stmt = $mysqli->prepare($ret);
+                                        $stmt->execute(); //ok
+                                        $res = $stmt->get_result();
+                                        while ($enrollments = $res->fetch_object()) { ?>
+                                            <tr>
+                                                <td><?php echo $enrollments->std_regno; ?></td>
+                                                <td><?php echo $enrollments->std_name; ?></td>
+                                                <td><?php echo $enrollments->unit_code; ?></td>
+                                                <td><?php echo $enrollments->unit_name; ?></td>
+                                                <td><?php echo $enrollments->semester_enrolled; ?></td>
+                                                <td><?php echo $enrollments->academic_year_enrolled; ?></td>
+                                                <td>
+                                                    <a href="#generate-<?php echo $enrollments->id; ?>" data-toggle="modal" class="badge outline-badge-danger">Generate Exam Card</a>
+                                                    <!-- Delete Modal -->
+                                                    <div class="modal animated zoomInUp custo-zoomInUp" id="generate-<?php echo $enrollments->id; ?>" role="dialog">
+                                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">CONFIRM</h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body text-center text-danger">
+                                                                    <h4>Delete <?php echo $enrollments->std_name; ?>'s enrollment ? </h4>
+                                                                    <br>
+                                                                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                    <a href="enrollments.php?delete=<?php echo $enrollments->id; ?>" class="text-center btn btn-danger"> Delete </a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- End Modal -->
+                                                </td>
+                                            </tr>
+                                        <?php }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php require_once '../partials/footer.php'; ?>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+    <!-- END MAIN CONTAINER -->
+    <?php require_once '../partials/scripts.php'; ?>
+</body>
+
+</html>
