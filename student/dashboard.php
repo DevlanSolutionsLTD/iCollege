5f8630228aa591098e514f80b44f3536c7b9a8e7
MartMbithi <martdevelopers254@gmail.com>
2021-02-13 11:06:51 +0300

    Student Auth Scaffs

---
+++ student/dashboard.php
@@ -0,0 +1,190 @@
+<?php
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+staff();
+require_once('../partials/analytics.php');
+require_once('../partials/head.php');
+?>
+
+<body>
+    <!-- BEGIN LOADER -->
+    <div id="load_screen">
+        <div class="loader">
+            <div class="loader-content">
+                <div class="spinner-grow align-self-center"></div>
+            </div>
+        </div>
+    </div>
+    <!--  END LOADER -->
+
+    <!--  BEGIN NAVBAR  -->
+    <?php require_once('../partials/admin_nav.php'); ?>
+    <!--  END NAVBAR  -->
+
+    <!--  BEGIN Breadcrumps Nav  -->
+    <div class="sub-header-container">
+        <header class="header navbar navbar-expand-sm">
+            <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg></a>
+
+            <ul class="navbar-nav flex-row">
+                <li>
+                    <div class="page-header">
+
+                        <nav class="breadcrumb-one" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><span>Dashboard</span></li>
+                            </ol>
+                        </nav>
+
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav flex-row ml-auto ">
+                <li class="nav-item more-dropdown">
+                    <div class="dropdown  custom-dropdown-icon">
+                        <a class="dropdown-toggle btn" href="#" role="button" id="customDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span>Short Links</span> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down">
+                                <polyline points="6 9 12 15 18 9"></polyline>
+                            </svg></a>
+
+                    </div>
+                </li>
+            </ul>
+        </header>
+    </div>
+    <!--  END Bread crupms Navbar  -->
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+        <!--  BEGIN SIDEBAR  -->
+        <?php require_once('../partials/staff_sidebar.php'); ?>
+        <!--  END SIDEBAR  -->
+
+        <!--  BEGIN CONTENT PART  -->
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                <div class="row layout-top-spacing">
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-3 col-12 layout-spacing">
+                        <a href="allocations.php">
+                            <div class="widget widget-card-four">
+                                <div class="widget-content">
+                                    <div class="w-content">
+                                        <div class="w-info">
+                                            <h6 class="value">Allocated Units</h6>
+                                        </div>
+                                        <div class="">
+                                            <div class="w-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                    <line x1="8" y1="6" x2="21" y2="6"></line>
+                                                    <line x1="8" y1="12" x2="21" y2="12"></line>
+                                                    <line x1="8" y1="18" x2="21" y2="18"></line>
+                                                    <line x1="3" y1="6" x2="3" y2="6"></line>
+                                                    <line x1="3" y1="12" x2="3" y2="12"></line>
+                                                    <line x1="3" y1="18" x2="3" y2="18"></line>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-3 col-12 layout-spacing">
+                        <a href="enrollments.php">
+                            <div class="widget widget-card-four">
+                                <div class="widget-content">
+                                    <div class="w-content">
+                                        <div class="w-info">
+                                            <h6 class="value">Enrolled Students</h6>
+                                        </div>
+                                        <div class="">
+                                            <div class="w-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                                    <circle cx="8.5" cy="7" r="4"></circle>
+                                                    <polyline points="17 11 19 13 23 9"></polyline>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-3 col-12 layout-spacing">
+                        <a href="marks_entry.php">
+                            <div class="widget widget-card-four">
+                                <div class="widget-content">
+                                    <div class="w-content">
+                                        <div class="w-info">
+                                            <h6 class="value">Exam Marks Entry</h6>
+                                        </div>
+                                        <div class="">
+                                            <div class="w-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
+                                                    <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+                                                    <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-3 col-12 layout-spacing">
+                        <a href="user_profile.php">
+                            <div class="widget widget-card-four">
+                                <div class="widget-content">
+                                    <div class="w-content">
+                                        <div class="w-info">
+                                            <h6 class="value">My Profile</h6>
+                                        </div>
+                                        <div class="">
+                                            <div class="w-icon">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                                    <circle cx="12" cy="7" r="4"></circle>
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div class="col-xl-12 col-lg-4 col-md-4 col-sm-4 col-12 layout-spacing">
+                        <!-- Draw Something Here -->
+                    </div>
+                </div>
+            </div>
+
+            <?php require_once('../partials/footer.php'); ?>
+        </div>
+        <!--  END CONTENT PART  -->
+
+    </div>
+    <!-- END MAIN CONTAINER -->
+    <?php require_once('../partials/scripts.php'); ?>
+
+</body>
+
+</html>
