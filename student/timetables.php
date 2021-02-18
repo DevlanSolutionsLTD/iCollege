22dc32a4f39489f37634bb089d3c7439b00e3feb
MartMbithi <martdevelopers254@gmail.com>
2021-02-18 11:12:00 +0300

    Invoked StudentCheckLogin()-Fixed Loss Session Bug

--- student/timetables.php
+++ student/timetables.php
@@ -2,7 +2,7 @@
 session_start();
 require_once '../config/config.php';
 require_once '../config/checklogin.php';
-admin_check_login();
+student();
 require_once '../config/codeGen.php';
 require_once '../partials/head.php';
 ?>
