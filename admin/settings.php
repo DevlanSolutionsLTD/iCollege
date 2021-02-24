7461f30830d40a39337394ee98f1111462fd710d
MartMbithi <martdevelopers254@gmail.com>
2021-02-24 12:19:16 +0300

    Injected System Update Details Backend Code

--- admin/settings.php
+++ admin/settings.php
@@ -4,6 +4,58 @@ require_once '../config/config.php';
 require_once '../config/checklogin.php';
 admin_check_login();
 require_once '../config/codeGen.php';
+/* Inject System Settings BackEnd Logic */
+
+
+if (isset($_POST['Save_System_Details'])) {
+
+    /* System Settings */
+    $error = 0;
+    if (isset($_POST['sys_name']) && !empty($_POST['sys_name'])) {
+        $sys_name = mysqli_real_escape_string($mysqli, trim($_POST['sys_name']));
+    } else {
+        $error = 1;
+        $err = 'System Name Cannot Be  Empty';
+    }
+
+    if (isset($_POST['sys_tagline']) && !empty($_POST['sys_tagline'])) {
+        $sys_tagline = mysqli_real_escape_string($mysqli, trim($_POST['sys_tagline']));
+    } else {
+        $error = 1;
+        $err = 'System Tagline Cannot  Be Empty';
+    }
+
+    if (isset($_POST['sys_about']) && !empty($_POST['sys_about'])) {
+        $sys_about = mysqli_real_escape_string($mysqli, trim($_POST['sys_about']));
+    } else {
+        $error = 1;
+        $err = 'System About Cannot Be Empty';
+    }
+
+    if (isset($_POST['sys_id']) && !empty($_POST['sys_id'])) {
+        $sys_id = mysqli_real_escape_string($mysqli, trim($_POST['sys_id']));
+    } else {
+        $error = 1;
+        $err = 'System ID Cannot Be Empty';
+    }
+
+
+    /* Load System Logo */
+    $sys_logo = $_FILES['sys_logo']['name'];
+    move_uploaded_file($_FILES["sys_logo"]["tmp_name"], "../public/uploads/sys_logo/" . $_FILES["sys_logo"]["name"]);
+    $query = 'UPDATE iCollege_Settings  SET  sys_name =? ,sys_logo =? ,sys_tagline =?, sys_about =?  WHERE sys_id =?';
+    $stmt = $conn->prepare($query);
+    $rc = $stmt->bind_param('sssss', $sys_name, $sys_logo, $sys_tagline, $sys_about, $sys_id);
+    $stmt->execute();
+    if ($stmt) {
+        $success = 'System Settings Updated' && header('refresh:1; url=settings.php');
+    } else {
+        //inject alert that task failed
+        $info = 'Please Try Again Or Try Later';
+    }
+}
+
+/* System Contact Details */
 require_once '../partials/head.php';
 ?>
 
@@ -85,89 +137,98 @@ require_once '../partials/head.php';
                                         </li>
 
                                     </ul>
-                                    <div class="tab-content" id="rounded-pills-icon-tabContent">
-                                        <div class="tab-pane fade show active" id="rounded-pills-icon-settings" role="tabpanel" aria-labelledby="rounded-pills-icon-home-tab">
-                                            <form method="post" enctype="multipart/form-data">
-                                                <div class="card-body">
-                                                    <div class="row">
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">System Name</label>
-                                                            <input type="text" required name="sys_name" value="<?php echo $sys->sys_name; ?>" class="form-control">
-                                                            <!-- Hide This -->
-                                                            <input type="hidden" required name="id" value="<?php echo $sys->sys_id; ?>" class="form-control">
-                                                        </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="exampleInputFile">System Logo</label>
-                                                            <div class="input-group">
-                                                                <div class="custom-file">
-                                                                    <input required name="file" accept=".png, .svg, .jpeg, .jpg" type="sys_logo" class="custom-file-input" id="exampleInputFile">
-                                                                    <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                    <?php
+                                    /* Load System Settings */
+                                    $ret = 'SELECT * FROM `iCollege_Settings`';
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute(); //ok
+                                    $res = $stmt->get_result();
+                                    while ($sys = $res->fetch_object()) {
+                                    ?>
+                                        <div class="tab-content" id="rounded-pills-icon-tabContent">
+                                            <div class="tab-pane fade show active" id="rounded-pills-icon-settings" role="tabpanel" aria-labelledby="rounded-pills-icon-home-tab">
+                                                <form method="post" enctype="multipart/form-data">
+                                                    <div class="card-body">
+                                                        <div class="row">
+                                                            <div class="form-group col-md-6">
+                                                                <label for="">System Name</label>
+                                                                <input type="text" required name="sys_name" value="<?php echo $sys->sys_name; ?>" class="form-control">
+                                                                <!-- Hide This -->
+                                                                <input type="hidden" required name="id" value="<?php echo $sys->sys_id; ?>" class="form-control">
+                                                            </div>
+                                                            <div class="form-group col-md-6">
+                                                                <label for="exampleInputFile">System Logo</label>
+                                                                <div class="input-group">
+                                                                    <div class="custom-file">
+                                                                        <input required name="file" accept=".png, .svg, .jpeg, .jpg" type="sys_logo" class="custom-file-input" id="exampleInputFile">
+                                                                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                                                    </div>
                                                                 </div>
                                                             </div>
-                                                        </div>
 
-                                                        <div class="form-group col-md-12">
-                                                            <label for="">System Tagline</label>
-                                                            <input type="text" required name="sys_tagline" value="<?php echo $sys->sys_tagline; ?>" class="form-control">
-                                                        </div>
+                                                            <div class="form-group col-md-12">
+                                                                <label for="">System Tagline</label>
+                                                                <input type="text" required name="sys_tagline" value="<?php echo $sys->sys_tagline; ?>" class="form-control">
+                                                            </div>
 
-                                                        <div class="form-group col-md-12">
-                                                            <label for="exampleInputPassword1">System About</label>
-                                                            <textarea required name="sys_about" rows="7" class="form-control"><?php echo $sys->sys_about; ?></textarea>
-                                                        </div>
+                                                            <div class="form-group col-md-12">
+                                                                <label for="exampleInputPassword1">System About</label>
+                                                                <textarea required name="sys_about" rows="7" class="form-control"><?php echo $sys->sys_about; ?></textarea>
+                                                            </div>
 
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="text-right">
-                                                    <button type="submit" name="Save_System_Details" class="btn btn-primary">Submit</button>
-                                                </div>
-                                            </form>
-                                        </div>
-
-                                        <div class="tab-pane fade" id="rounded-pills-icon-contact" role="tabpanel" aria-labelledby="rounded-pills-icon-contact-tab">
-                                            <form method="post" enctype="multipart/form-data">
-                                                <div class="card-body">
-                                                    <div class="row">
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Email Address</label>
-                                                            <input type="text" required name="sys_mail" value="<?php echo $sys->sys_mail; ?>" class="form-control">
-                                                            <!-- Hide This -->
-                                                            <input type="hidden" required name="id" value="<?php echo $sys->sys_id; ?>" class="form-control">
-                                                        </div>
-                                                        <div class="form-group col-md-6">
-                                                            <label for="">Mobile Phone Number</label>
-                                                            <input type="text" required name="sys_phone_contact" value="<?php echo $sys->sys_phone_contact; ?>" class="form-control">
-                                                        </div>
-
-                                                        <div class="form-group col-md-4">
-                                                            <label for="">Facebook Page Url / Username</label>
-                                                            <input type="text" required name="sys_fb" value="<?php echo $sys->sys_fb; ?>" class="form-control">
-                                                        </div>
-
-                                                        <div class="form-group col-md-4">
-                                                            <label for="">Instagram Page Url / Username</label>
-                                                            <input type="text" required name="sys_ig" value="<?php echo $sys->sys_ig; ?>" class="form-control">
-                                                        </div>
-
-                                                        <div class="form-group col-md-4">
-                                                            <label for="">Twitter Username</label>
-                                                            <input type="text" required name="sys_twitter" value="<?php echo $sys->sys_twitter; ?>" class="form-control">
-                                                        </div>
-
-                                                        <div class="form-group col-md-12">
-                                                            <label for="exampleInputPassword1">Google Maps Embed Map Url</label>
-                                                            <textarea required name="sys_googlemap" rows="4" class="form-control"></textarea>
-                                                        </div>
-
+                                                    <div class="text-right">
+                                                        <button type="submit" name="Save_System_Details" class="btn btn-primary">Submit</button>
                                                     </div>
-                                                </div>
-                                                <div class="text-right">
-                                                    <button type="submit" name="Save_Contact_Details" class="btn btn-primary">Submit</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
+                                                </form>
+                                            </div>
 
+                                            <div class="tab-pane fade" id="rounded-pills-icon-contact" role="tabpanel" aria-labelledby="rounded-pills-icon-contact-tab">
+                                                <form method="post" enctype="multipart/form-data">
+                                                    <div class="card-body">
+                                                        <div class="row">
+                                                            <div class="form-group col-md-6">
+                                                                <label for="">Email Address</label>
+                                                                <input type="text" required name="sys_mail" value="<?php echo $sys->sys_mail; ?>" class="form-control">
+                                                                <!-- Hide This -->
+                                                                <input type="hidden" required name="id" value="<?php echo $sys->sys_id; ?>" class="form-control">
+                                                            </div>
+                                                            <div class="form-group col-md-6">
+                                                                <label for="">Mobile Phone Number</label>
+                                                                <input type="text" required name="sys_phone_contact" value="<?php echo $sys->sys_phone_contact; ?>" class="form-control">
+                                                            </div>
+
+                                                            <div class="form-group col-md-4">
+                                                                <label for="">Facebook Page Url / Username</label>
+                                                                <input type="text" required name="sys_fb" value="<?php echo $sys->sys_fb; ?>" class="form-control">
+                                                            </div>
+
+                                                            <div class="form-group col-md-4">
+                                                                <label for="">Instagram Page Url / Username</label>
+                                                                <input type="text" required name="sys_ig" value="<?php echo $sys->sys_ig; ?>" class="form-control">
+                                                            </div>
+
+                                                            <div class="form-group col-md-4">
+                                                                <label for="">Twitter Username</label>
+                                                                <input type="text" required name="sys_twitter" value="<?php echo $sys->sys_twitter; ?>" class="form-control">
+                                                            </div>
+
+                                                            <div class="form-group col-md-12">
+                                                                <label for="exampleInputPassword1">Google Maps Embed Map Url</label>
+                                                                <textarea required name="sys_googlemap" rows="4" class="form-control"></textarea>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                    <div class="text-right">
+                                                        <button type="submit" name="Save_Contact_Details" class="btn btn-primary">Submit</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    <?php
+                                    } ?>
                                 </div>
                             </div>
                         </div>
